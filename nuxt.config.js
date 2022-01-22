11ab4e5024637596f3e1b550213b26d1ddb02554
James Totty <jt.totty1991@gmail.com>
2022-01-22 20:53:32 +0000

    :hammer: Using Vite to compile.

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,8 +34,9 @@ export default {
 
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
-    // https://go.nuxtjs.dev/eslint
-        '@nuxtjs/eslint-module'
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+        'nuxt-vite'
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
