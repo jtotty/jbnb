b591287357e336026b28882ecdcdad6027775905
James Totty <jt.totty1991@gmail.com>
2022-01-23 17:33:50 +0000

    :construction: HomeRow component

--- pages/searchCity.vue
+++ pages/searchCity.vue
@@ -1,14 +1,26 @@
 <template>
     <div>
-        {{ lat }} / {{ lng }} / {{ label }}
-        <div v-for="home in homes" :key="home.objectID">
-            {{ home.title }}<br>
+        {{ lat }} / {{ lng }} / {{ label }}<br>
+        <div v-if="homes.length > 0">
+            <HomeRow v-for="home in homes" :key="home.objectID" :home="home" />
+        </div>
+        <div v-else>
+            No results found.
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    async beforeRouteUpdate(to, from, next) {
+        const data = await this.$dataApi.getHomeByLocation(to.query.lat, to.query.lng)
+        this.homes = data.json.hits
+        this.label = to.query.label
+        this.lat = to.query.lat
+        this.lng = to.query.lng
+        next()
+    },
+
     async asyncData({ query, $dataApi }) {
         const data = await $dataApi.getHomeByLocation(query.lat, query.lng)
 
@@ -20,6 +32,10 @@ export default {
         }
     },
 
-    watchQuery: ['lat']
+    head() {
+        return {
+            title: `Homes around ${this.label}`
+        }
+    }
 }
 </script>
